680d3c0cd4555beda23f83fc7f535390a3ad5df1
Aditya Soni <adityasoni9727@gmail.com>
2022-10-12 00:54:40 +0530

    Adding the react router thing.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () =>(
+  <div>
+  <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      {/* <Route path = '/' component= {HomePage}/>
+      <Route path = '/hats' component= {HatsPage}/> */}
+      <Route path="/user" component={HomePage}/>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <HomePage/>
+//     </div>
+//   );
+// }
 
 export default App;
