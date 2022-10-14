54073aa9287529ea333841f2b4287702a98de358
Aditya Soni <adityasoni9727@gmail.com>
2022-10-14 16:49:37 +0530

    Hello. Added react-router-dom dummy working.
    Co-authored-by: Hemendra sharma <CodeNinja04@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react' 
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component';
+import HelloItem from "./pages/hello/hello";
 
-function App() {
+import './App.css';
+const App = () => {
+
+  
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    //   <Link to="/" index element={<HomePage />} />
+    //   <Link to='/aditya' index element={<HomePage />} />
+    //   <Link to='/hhh' index element={<MenuItem />} />
+    // <R
+    <Routes>
+      <Route path="/hoes" element={<HomePage />} />
+      <Route path="/hoe" element={<HelloItem />} />
+    </Routes>
+
   );
-}
+};
 
 export default App;
---
+++ src/pages/hello/hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloItem = ()=>(
+    <div>Hello, this is hoe.</div>
+);
+
+export default HelloItem;
