725caf7639baddde83695384d8f8e675b54a6567
Aditya Soni <adityasoni9727@gmail.com>
2022-10-15 01:21:39 +0530

    Completed the Mod. 5. Now moving to 6.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
-import HelloItem from "./pages/hello/hello";
-import HelloItem2 from "./pages/hello/hello2";
+// import HomePage from './pages/homepage/homepage.component';
+
+const HomePage = (props) =>{
+  console.log(props);
+  return (
+    <div>
+      <Link to = "/user1">Go to page 1</Link>
+      <h1>HomePage</h1>
+    </div>
+  )
+}
+const HelloItem1 = (props) =>{
+  console.log(props);
+  
+  return (
+    <div>
+      <h1>HelloItem1</h1>
+    </div>
+  )
+}
+
+const HelloItem2 = (props) =>{
+  console.log(props);
+  return (
+    <div>
+      <h1>HelloItem2</h1>
+    </div>
+  )
+}
 
 const App = () => {
 
   return (
     
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/user1" element={<HelloItem />} />
+      <Route path="" element={<HomePage />} />
+      <Route path="/user1" element={<HelloItem1 />} />
       <Route path="/user2" element={<HelloItem2 />} />
     </Routes>
   );
--- src/pages/hello/hello.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HelloItem = ()=>(
-    <div>Hello, this is page1..</div>
-);
-
-export default HelloItem;
