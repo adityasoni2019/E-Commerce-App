2194f9fd29f829fc04c8babbebff3f6fff58a258
Aditya Soni <adityasoni9727@gmail.com>
2022-10-14 23:39:36 +0530

    Finally done with adding the react-router-dom successfully.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import HomePage from './pages/homepage/homepage.component';
 import HelloItem from "./pages/hello/hello";
+import HelloItem2 from "./pages/hello/hello2";
 
-import './App.css';
 const App = () => {
 
-  
   return (
-    //   <Link to="/" index element={<HomePage />} />
-    //   <Link to='/aditya' index element={<HomePage />} />
-    //   <Link to='/hhh' index element={<MenuItem />} />
-    // <R
+    
     <Routes>
-      <Route path="/hoes" element={<HomePage />} />
-      <Route path="/hoe" element={<HelloItem />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/user1" element={<HelloItem />} />
+      <Route path="/user2" element={<HelloItem2 />} />
     </Routes>
-
   );
 };
 
--- src/pages/hello/hello.jsx
+++ src/pages/hello/hello.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HelloItem = ()=>(
-    <div>Hello, this is hoe.</div>
+    <div>Hello, this is aditya..</div>
 );
 
 export default HelloItem;
